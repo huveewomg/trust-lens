ce0ff88fbba6b3150c113167b5ccaf481e26e617
Huvee <wongrenthou1265@gmail.com>
2025-09-14 22:45:20 +0800

    google cloud run related changes

--- my-fastapi-app/app/main.py
+++ my-fastapi-app/app/main.py
@@ -10,6 +10,7 @@ app.add_middleware(
         "http://localhost:8080",
         "http://127.0.0.1:8000",
         "null"
+        "https://huveewomg.github.io/trust-lens/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
