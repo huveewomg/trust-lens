fd811258b1564c1930721f2a6fc90d37c4a90594
Huvee <wongrenthou1265@gmail.com>
2025-09-15 22:43:29 +0800

    update endpoint

--- static/js/script.js
+++ static/js/script.js
@@ -69,7 +69,7 @@ elements.analyzeButton.addEventListener('click', async () => {
   updateStatus('Analyzing with AI… <span class="spinner"></span>');
 
   try {
-    const response = await fetch('https://trust-lens-703020842709.asia-southeast1.run.app/predict/', {
+    const response = await fetch('https://trust-lens-api-703020842709.asia-southeast1.run.app/predict/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
